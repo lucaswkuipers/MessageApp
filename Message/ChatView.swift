a266cb7c8e18347bd2f29341304e012b4eef8a00
Lucas Werner Kuipers <lucaswkuipers@gmail.com>
2022-04-20 04:52:38 -0300

    Add messages stack view

--- Message/ChatView.swift
+++ Message/ChatView.swift
@@ -1,6 +1,16 @@
 import UIKit
 
 final class ChatView: UIView {
+    let messagesStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        return stackView
+    }()
+
+    let messagesScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
 
     init() {
         super.init(frame: .zero)
@@ -15,6 +25,16 @@ final class ChatView: UIView {
     }
 
     private func setupViewStyle() {}
-    private func setupViewHierarchy() {}
-    private func setupViewConstraints() {}
+    private func setupViewHierarchy() {
+        addSubview(messagesStackView)
+    }
+    private func setupViewConstraints() {
+        subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
+        NSLayoutConstraint.activate([
+            messagesStackView.topAnchor.constraint(equalTo: topAnchor),
+            messagesStackView.leftAnchor.constraint(equalTo: leftAnchor),
+            messagesStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            messagesStackView.rightAnchor.constraint(equalTo: rightAnchor),
+        ])
+    }
 }
